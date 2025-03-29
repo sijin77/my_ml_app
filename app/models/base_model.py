398af0f7b2cb4652c372717113bf19f265e55d30
Marat Darchiev <darchievmb@gmail.com>
2025-03-29 14:34:37 +0300

    fixed docker compose

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -1,3 +1,13 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import declared_attr
 
-Base = declarative_base()
+from config.app_config import settings
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
